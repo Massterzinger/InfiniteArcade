2c004ed932f9a950d0650ba1e240099ea675989d
Richard Tyshchenko <wromankosawp@gmail.com>
2017-03-18 21:39:24 +0200

    refactored parallax

--- Assets/Scripts/Parallaxer.cs
+++ Assets/Scripts/Parallaxer.cs
@@ -2,25 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallaxer : MonoBehaviour {
-    public Material FrontBlock;
-    public Material BackMountain;
-    public Material MidMountain;
-    public Material FrontMountain;
+public class Parallaxer : MonoBehaviour
+{
+    public float Parallax;
 
-    public float FrontBlockParallax, BackMountainParallax, MidMountainParallax, FrontMountainParallax;
+    private MeshRenderer mr;
+    private Material mat;
+    private Vector2 offset;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        FrontBlock.mainTextureOffset = new Vector2( FrontBlock.mainTextureOffset.x+Time.deltaTime* FrontBlockParallax, FrontBlock.mainTextureOffset.y);
-        BackMountain.mainTextureOffset = new Vector2(BackMountain.mainTextureOffset.x + Time.deltaTime * BackMountainParallax, BackMountain.mainTextureOffset.y);
-        MidMountain.mainTextureOffset = new Vector2(MidMountain.mainTextureOffset.x + Time.deltaTime * MidMountainParallax, MidMountain.mainTextureOffset.y);
-        FrontMountain.mainTextureOffset = new Vector2(FrontMountain.mainTextureOffset.x + Time.deltaTime * FrontMountainParallax, FrontMountain.mainTextureOffset.y);
-
+    // Update is called once per frame
+    void Update()
+    {
+        mr = gameObject.GetComponent<MeshRenderer>();
+        mat = mr.material;
+        offset = mat.mainTextureOffset;
+        offset.x += Parallax * Time.deltaTime;
+        mat.mainTextureOffset = offset;
     }
 }
